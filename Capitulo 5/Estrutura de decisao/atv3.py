1adf44f387ac89493230b1b0d89ee741afa502df
danezi <danezi.maria@escola.pr.gov.br>
2025-09-30 11:52:54 -0300

    lista2

---
+++ Capitulo 5/Estrutura de decisao/atv3.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite o primeiro numero"))
+numero2 = int(input("Digite o segundo numero"))
+numero3 = int(input("Digite o terceiro numero"))
+
+if numero>numero2:
+    print("numero maior que numero2")
+if numero2>numero3:
+    print("numero2 maior que numero3")
+if numero2>numero3:
+
+
